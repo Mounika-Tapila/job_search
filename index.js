fcf5c923f03b49ea956860a96066147d685bd337
Mounika-Tapila <t.mounisudha@gmail.com>
2026-05-30 11:32:48 +0530

    add mongodb error debug

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected ✅"))
-  .catch((err) => console.log(err));
-
+  .catch((err) => {
+    console.log("MongoDB Error ❌:", err);
+  });
 /* ---------------- HOME ---------------- */
 
 app.get("/", (req, res) => {
